29ba3a215a860377f5cb67d236cc750e311eca99
abimm1902 <144413968+abimm1902@users.noreply.github.com>
2026-08-18 16:08:26 +0530

    Remove comment from Roles decorator
    Remove comment explaining the Roles decorator.

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '../enums/role.enum';
 
-// Attaches allowed roles to a route, e.g. @Roles(Role.ADMIN)
 export const Roles = (...roles: Role[]) => SetMetadata('roles', roles);
